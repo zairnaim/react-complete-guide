5d71079e21b15c9e6d2c8ff6bf440a8f695e4fab
zairnaim <zairnaim@hotmail.com>
2019-12-21 22:15:54 -0500

    inline style for button

--- src/App.js
+++ src/App.js
@@ -36,13 +36,21 @@ class App extends Component {
   }
 
   render() {
+    const mybuttonstyle = {
+      backgroundColor: 'White',
+      font: 'inherit',
+      border: '1px solid red',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>
           Hi I'm a react app
         </h1>
         <p>This is really working</p>
-        <button onClick={() => this.buttonPressHandler("Maximillionsss")}>I am a button</button>
+        <button style={mybuttonstyle} onClick={() => this.buttonPressHandler("Maximillionsss")}>I am a button</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name=
           {this.state.persons[1].name}
